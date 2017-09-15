ef25f2466dca4b4b6137db2d818bb94f6c7fdba8
Marsellus47 <mjackow@gmail.com>
2017-09-15 21:33:28 +0200

    Module 6 - Reacting to Changes

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import {
+    FormGroup,
+    FormBuilder,
+    Validators,
+    AbstractControl,
+    ValidatorFn
+} from '@angular/forms';
 
 import { Customer } from './customer';
+
+function emailMatcher(c: AbstractControl) {
+    const emailControl = c.get('email');
+    const confirmControl = c.get('confirmEmail');
+    if (emailControl.pristine || confirmControl.pristine) {
+        return null;
+    }
+    if (emailControl.value === confirmControl.value) {
+        return null;
+    }
+    return { 'match': true };
+}
+
+function ratingRange(min: number, max: number): ValidatorFn {
+    return (c: AbstractControl): { [key: string]: boolean } | null => {
+        if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+            return { 'range': true };
+        }
+        return null;
+    };
+}
 
 @Component({
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent implements OnInit  {
+export class CustomerComponent implements OnInit {
     customerForm: FormGroup;
     customer: Customer = new Customer();
-    private initialData: object;
+    private initialData: Object;
 
     constructor(private fb: FormBuilder) { }
 
@@ -21,10 +48,16 @@ export class CustomerComponent implements OnInit  {
 
     ngOnInit(): void {
         this.initialData = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            sendCatalog: { value: true, disabled: false }
+            firstName: ['', [Validators.required, Validators.minLength(3)]],
+            lastName: ['', [Validators.required, Validators.maxLength(50)]],
+            emailGroup: this.fb.group({
+                email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]],
+                confirmEmail: ['', Validators.required],
+            }, { validator: emailMatcher }),
+            phone: '',
+            notification: 'email',
+            rating: ['', ratingRange(1, 5)],
+            sendCatalog: { value: false, disabled: false }
         };
         this.customerForm = this.fb.group(this.initialData);
     }
@@ -47,4 +80,14 @@ export class CustomerComponent implements OnInit  {
     clearForm(): void {
         this.customerForm.setValue(this.initialData);
     }
- }
+
+    setNotification(notifyVia: string): void {
+        const phoneControl = this.customerForm.get('phone');
+        if (notifyVia === 'text') {
+            phoneControl.setValidators(Validators.required);
+        } else {
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
+    }
+}
